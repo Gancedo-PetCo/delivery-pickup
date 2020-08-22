a53d531bfd0f2b6c08643d4a01f90c9bb8d8d22d
Jeremy Chen <jeremylfchen@gmail.com>
2020-08-22 14:08:44 -0700

    install babel plugin transform run time for async testing

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 // aws config
-module.exports = {
-  mongoUri: 'mongodb://172.31.46.64:27017/itemAvailability',
-  itemPrice: 'http://52.14.208.55:3005/itemPrice/',
-  availableAt: 'http://18.224.229.28:3006/availableAt/'
-
-};
-
-//local config
 // module.exports = {
-//   mongoUri: 'mongodb://localhost/itemAvailability',
-//   itemPrice: 'http://127.0.0.1:3005/itemPrice/',
-//   availableAt: 'http://127.0.0.1:3006/availableAt/'
-// }
+//   mongoUri: 'mongodb://172.31.46.64:27017/itemAvailability',
+//   itemPrice: 'http://52.14.208.55:3005/itemPrice/',
+//   availableAt: 'http://18.224.229.28:3006/availableAt/'
+
+// };
+
+// local config
+module.exports = {
+  mongoUri: 'mongodb://localhost/itemAvailability',
+  itemPrice: 'http://127.0.0.1:3005/itemPrice/',
+  availableAt: 'http://127.0.0.1:3006/availableAt/'
+};
--- test/server.test.js
+++ test/server.test.js
@@ -1,52 +1,76 @@
 const insertRecords = require('../database-mongodb/insert_records.js');
-const { ItemAvailability, Store } = require('../database-mongodb/itemAvailability.js');
+const config = require('../config.js');
+const { ItemAvailability, Store, CrudOps } = require('../database-mongodb/itemAvailability.js');
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../server/index.js');
 
-beforeAll(() => {
-  return mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
+beforeAll(async () => {
+
+  await mongoose.connect(config.mongoUri, {
+    useNewUrlParser: true,
+    poolSize: 10
+  })
     .then(() => {
-      return ItemAvailability.remove({});
+      ItemAvailability.remove({});
     })
     .then(() => {
-      return Store.remove({});
+      Store.remove({});
     })
     .then(() => {
-      return insertRecords();
+      insertRecords();
     });
 });
 
-afterAll(() => {
-  return mongoose.connection.close();
-})
+afterAll(async () => {
+  await mongoose.connection.close();
+});
 
-test('successfully retrieves availability data for item 106', () => {
-  return ItemAvailability.findOne({ itemId: '106' })
-    .populate({
-      path: 'itemAvailability',
-      populate: {
-        path: 'storeId'
+// test('successfully retrieves availability data for item 106', () => {
+//   return ItemAvailability.findOne({ itemId: '106' })
+//     .populate({
+//       path: 'itemAvailability',
+//       populate: {
+//         path: 'storeId'
+//       }
+//     })
+//     .then((data) => {
+//       return request(app).get('/availableAt/106/')
+//         .then((response) => {
+//           // console.log(response.body);
+//           expect(response.status).toBe(200);
+//           expect(response.body.itemAvailability).toHaveLength(5);
+//           expect(response.body.itemAvailability[0].availability).toBe(data.itemAvailability[0].availability);
+//           expect(response.body.itemAvailability[0].storeName).toBe(data.itemAvailability[0].storeId.storeName);
+//           expect(response.body.itemAvailability[0].storeAddress).toBe(data.itemAvailability[0].storeId.storeAddress);
+//           expect(response.body.itemAvailability[0].storePhoneNumber).toBe(data.itemAvailability[0].storeId.storePhoneNumber);
+//         });
+//     });
+// });
+
+describe('BackEnd Tests', () => {
+  test('successfully retrieves availability data for item 106', () => {
+    let data = CrudOps.readItemAvailability('106', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        return data;
       }
-    })
-    .then((data) => {
-      return request(app).get('/availableAt/106/')
-        .then((response) => {
-          // console.log(response.body);
-          expect(response.status).toBe(200);
-          expect(response.body.itemAvailability).toHaveLength(5);
-          expect(response.body.itemAvailability[0].availability).toBe(data.itemAvailability[0].availability);
-          expect(response.body.itemAvailability[0].storeName).toBe(data.itemAvailability[0].storeId.storeName);
-          expect(response.body.itemAvailability[0].storeAddress).toBe(data.itemAvailability[0].storeId.storeAddress);
-          expect(response.body.itemAvailability[0].storePhoneNumber).toBe(data.itemAvailability[0].storeId.storePhoneNumber);
-        });
     });
-
-  test('returns 404 status code if item not found', () => {
-    return request(app).get('/availableAt/200')
-      .then((response) => {
-        expect(response.status).toBe(404);
-      });
+    expect(data).toHaveProperty('itemId');
+    expect(data).toHaveProperty('itemAvailability');
+    expect(data.itemAvailabilitylength).toBeGreaterThan(0);
   });
-
 });
+
+
+
+
+// describe('Requests Test', () => {
+//   test('returns 404 status code if item not found', () => {
+//     return request(app).get('/availableAt/200')
+//       .then((response) => {
+//         expect(response.status).toBe(404);
+//       });
+//   });
+// });
