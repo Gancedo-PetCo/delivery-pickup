f0b66e5fa06fad7874d63c0c011e40e6fc701643
Jeremy Chen <jeremylfchen@gmail.com>
2020-10-03 10:13:57 -0700

    implemented redis

--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
 const React = require('react');
 const express = require('express');
 const ReactDOMServer = require('react-dom/server');
-
-// const fs = require('fs');
+const redis = require("redis");
 const PATH = require('path');
+const cors = require('cors');
 const { getData, deleteData, updateData, addData } = require('../database-couchdb/index.js');
 
 require('@babel/register');
 require('dotenv').config({ path: PATH.join(__dirname, '..', '.env') });
 require('newrelic');
 
-const cors = require('cors');
+const { REDIS_PORT } = process.env;
 const app = express();
+const redis_client = redis.createClient(REDIS_PORT);
 
 app.use(cors());
 app.use(express.json());
@@ -56,6 +57,23 @@ const constructHTMLFromTemplate = function (body) {
   `;
 };
 
+const checkCache = (req, res, next) => {
+  const itemId = req.params.itemId;
+
+  redis_client.get(itemId, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(err);
+    }
+    if (data != null) {
+      // console.log('redis found it!');
+      res.send(data);
+    } else {
+      next();
+    }
+  })
+}
+
 app.use(express.static(__dirname + '/../react-client/templates'));
 app.get('/app/:id', (req, res) => {
 
@@ -64,23 +82,35 @@ app.get('/app/:id', (req, res) => {
   res.status(200).send(SSR);
 });
 
-app.get('/availableAt/:itemId', (req, res) => {
+app.get('/availableAt/:itemId', checkCache, async (req, res) => {
   let itemId = req.params.itemId;
   console.log('GET received! ID: ', itemId);
 
-  getData(itemId)
-    .then((data) => {
-      if (Object.keys(data).length === 0) {
-        res.sendStatus(404);
-      } else {
-        // console.log(data)
-        res.status(200).send(data);
-      }
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
+  try {
+    const data = await getData(itemId);
+    if (Object.keys(data).length === 0) {
+      res.sendStatus(404);
+    } else {
+      redis_client.setex(itemId, 3600, JSON.stringify(data));
+      return res.status(200).send(data);
+    }
+  } catch (err) {
+    console.error('aaa ', err);
+    return res.status(500).json(err);
+  }
 
+  // getData(itemId)
+  //   .then((data) => {
+  //     if (Object.keys(data).length === 0) {
+  //       res.sendStatus(404);
+  //     } else {
+  //       // console.log(data)
+  //       res.status(200).send(data);
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     res.status(500).send(err);
+  //   });
 });
 
 
